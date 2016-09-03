7e0777dc0d08cba39a66b91e1cd425c2744a292b
Matthias Palmér <matthias@metasolutions.se>
2016-09-03 11:39:35 +0200

    Detect code RDF/XML needs to look further, just in case initial xml declarations

--- apps/components/RDFDetect.js
+++ apps/components/RDFDetect.js
@@ -9,8 +9,8 @@ define([
     return function(rdf) {
         var report = {};
         if (typeof rdf === "string") {
-            var taste = rdf.substr(0,100);
-            if (taste.substr(0,100).toLowerCase().indexOf("<rdf:rdf") !== -1) {
+            var taste = rdf.substr(0,200);
+            if (taste.toLowerCase().indexOf("<rdf:rdf") !== -1) {
                 report.format = "rdf/xml";
                 try {
                     report.graph = converters.rdfxml2graph(rdf);

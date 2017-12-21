2ccde1b619064a570c2ea33d5c0133bf1efdf8b7
Matthias Palmér <matthias@metasolutions.se>
2017-12-21 12:39:30 +0100

    Exact material design activation

--- view/bmd/all.js
+++ view/bmd/all.js
@@ -15,13 +15,23 @@ define([
 
   // initializeMaterial is not called more than once per X ms
   const updateMaterial = (node) => {
-    if (jquery.material) {
-      jquery.material.ripples(node);
-      jquery.material.input(node);
-      jquery.material.checkbox(node);
-      jquery.material.radio(node);
-      jquery.material.togglebutton(node);
-    }
+      if (jquery.material) {
+	  jquery(node).find(jquery.material.options.withRipples).each((idx, el) => {
+	      jquery.material.ripples(el);
+	  });
+	  jquery(node).find(jquery.material.options.inputElements).each((idx, el) => {
+	      jquery.material.input(el);
+	  });
+	  jquery(node).find(jquery.material.options.checkboxElements).each((idx, el) => {
+	      jquery.material.checkbox(el);
+	  });
+	  jquery(node).find(jquery.material.options.radioElements).each((idx, el) => {
+	      jquery.material.radio(el);
+	  });
+	  jquery(node).find(jquery.material.options.togglebuttonElements).each((idx, el) => {
+	      jquery.material.togglebutton(el);
+	  });  
+      }
   };
 
   renderingContext.postEditorRenderer = function (fieldDiv, binding, context) {

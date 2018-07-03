1fa43567f016631ed987167e5055ecaa118e0756
Matthias Palmer <matthias@metasolutions.se>
2018-07-03 11:14:39 +0200

    Selectize needs to be launched on an input not a div

--- view/bmd/Selectize.js
+++ view/bmd/Selectize.js
@@ -7,7 +7,9 @@ define([
 ], (renderingContext, jquery, system, utils) => {
   renderingContext.renderSelect = function (fieldDiv, binding, context) {
     const formgroup = jquery('<div class="form-group selectizeException">').appendTo(fieldDiv);
-    const $select = jquery('<div class="form-control">').appendTo(formgroup);
+    const formcontrol = jquery('<div class="form-control">').appendTo(formgroup);
+    const $select = jquery('<input type="text">').appendTo(formcontrol);
+
     const items = [];
     // Sets the value if any
     if (binding.getValue()) {

13b6de688e4f3f4193a3950da9bb29488b5785f0
Valentino Hudhra <v.hudhra@gmail.com>
2018-08-08 10:20:59 +0200

    Fix typo

--- samples/chooser.js
+++ samples/chooser.js
@@ -4,7 +4,7 @@ require([
   show(binding, onSelect) {
     onSelect({ value: 'http://example.com/3', label: { en: 'Example 3' } });
   },
-  seearch(item, term) {
+  search(item, term) {
     return new Promise(success => success([
       { value: 'http://example.com/1', label: { en: 'Example 1' }, description: { en: 'hoppla' } },
       { value: 'http://example.com/2', label: { en: 'Example 2' } },

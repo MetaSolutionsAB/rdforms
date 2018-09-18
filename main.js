8645084d7fc98ab9f030c5d50fe93e5c30f10360
Valentino Hudhra <v.hudhra@gmail.com>
2018-09-18 14:00:44 +0200

    Remove duplicate export

--- main.js
+++ main.js
@@ -6,6 +6,4 @@ export {default as Presenter} from './src/view/Presenter';
 export {default as Editor} from './src/view/Editor';
 export {default as LevelEditor} from './src/view/bootstrap/LevelEditor';
 export {default as ValidationPresenter} from './src/view/ValidationPresenter';
-export {default as Presenter} from './src/view/Presenter';
 export {default as utils} from './src/view/utils';
-

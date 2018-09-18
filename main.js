25b37f02c267e52b27d5c874dea3ac0b99721a64
Valentino Hudhra <v.hudhra@gmail.com>
2018-09-18 10:04:47 +0200

    Export more to facilicate import in ES

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+import './src/view/bmd/all'; // hard coded bmd
+
 export {default as ItemStore} from './src/template/ItemStore';
 export {default as bundleLoader} from './src/template/bundleLoader';
 export {default as Presenter} from './src/view/Presenter';
 export {default as Editor} from './src/view/Editor';
+export {default as LevelEditor} from './src/view/bootstrap/LevelEditor';
+export {default as ValidationPresenter} from './src/view/ValidationPresenter';
+export {default as Presenter} from './src/view/Presenter';
 export {default as utils} from './src/view/utils';
+

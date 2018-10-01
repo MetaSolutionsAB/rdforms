c698ad62f17852208ad8f90ef39204970ac1e4b2
Matthias Palmer <matthias@metasolutions.se>
2018-10-01 18:25:28 +0200

    Wrap long lines

--- view/bootstrap/text.js
+++ view/bootstrap/text.js
@@ -57,23 +57,28 @@ define([
     }
     const text = binding.getGist().replace('<', '&lt;').replace('>', '&gt;').replace(/(\r\n|\r|\n)/g, '<br/>');
 
-        // The text is shown as a link to the parents bindings URI if:
-        // 1) The current item is indicated to be a label.
-        // 2) The presenter is not at topLevel.
-        // 3) The current item is first in the parents list of children.
-        // 4) The parent binding corresponds to a URI.
+    // The text is shown as a link to the parents bindings URI if:
+    // 1) The current item is indicated to be a label.
+    // 2) The presenter is not at topLevel.
+    // 3) The current item is first in the parents list of children.
+    // 4) The parent binding corresponds to a URI.
     const parentBinding = binding.getParent();
     if (binding.getItem().hasStyle('label')
       && this.topLevel !== true
       && parentBinding != null && parentBinding.getItem().getChildren()[0] === binding.getItem()
       && parentBinding.getStatement() != null && parentBinding.getStatement().getType() === 'uri') {
       const $a = jquery('<a class="rdformsUrl">')
-                .attr('href', parentBinding.getStatement().getValue())
-                .html(text)
-                .appendTo(fieldDiv);
+        .attr('href', parentBinding.getStatement().getValue())
+        .html(text)
+        .appendTo(fieldDiv);
       system.attachLinkBehaviour($a[0], parentBinding);
     } else {
       jquery('<div>').toggleClass('rdformsField', context.inEditor).html(text).appendTo(fieldDiv);
+    }
+    if (binding.getItem().hasStyle('multiline')) {
+      jquery(fieldDiv).toggleClass('rdformsMultiline', true);
+    } else {
+      jquery(fieldDiv).toggleClass('rdformsSingleline', true);
     }
   });
 

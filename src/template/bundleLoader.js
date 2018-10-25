19efad0b171e86069cc16ffa4557af2dbb5bb35a
Valentino Hudhra <v.hudhra@gmail.com>
2018-10-25 17:36:41 +0200

    Make bundleloader agnostic of how the template url is constructed

--- src/template/bundleLoader.js
+++ src/template/bundleLoader.js
@@ -3,17 +3,10 @@ import {endsWith} from 'lodash-es';
 const isNode = require('detect-node');
 
 /**
- *
- * @param b
+ * @param {String} url
  * @returns {Promise<Response | never | void>}
  */
-const fetchBundle = (path) => {
-  path = `/${path}`; // TODO remove!
-  if (!endsWith(path, '.json')) {
-    path += '.json';
-  }
-  return fetch(path).then(r => r.json()).catch(e => console.log(`Fetching template bundle ${path} failed`, e))
-}
+const fetchBundle = url => fetch(url).then(r => r.json()).catch(e => console.log(`Fetching template bundle ${url} failed`, e));
 
 export default (itemStore, bundlePaths = [], callback = null) => {
   if (bundlePaths.length === 0) {
@@ -22,7 +15,10 @@ export default (itemStore, bundlePaths = [], callback = null) => {
   }
 
   const registerBundles = (bundlesJSON = []) => {
-    const bundles = bundlesJSON.map((bundle, idx) => itemStore.registerBundle({path: bundlePaths[idx], source: bundle}));
+    const bundles = bundlesJSON.map((bundle, idx) => itemStore.registerBundle({
+      path: bundlePaths[idx],
+      source: bundle
+    }));
     callback && callback(bundles);
   };
 

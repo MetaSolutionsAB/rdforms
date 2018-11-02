73ac2f6c54241f0e8483264376d34631d9d1fa50
Valentino Hudhra <v.hudhra@gmail.com>
2018-11-02 11:58:11 +0100

    Upadat previous examples

--- samples/example-loader.js
+++ samples/example-loader.js
@@ -1,17 +1,16 @@
 import rdfGraph from './rdf.js';
 import templateBundle from './templateBundle.js';
-import bundleMix from './bundleMix.js';
+import defaultBundles from './defaultBundles.js';
 
 const itemStore = new rdforms.ItemStore();
-//
-// const callback = (bundles) => {
-//   new rdforms.Editor({
-//     graph,
-//     resource: 'http://example.org/about',
-//     template: bundles[0].getRoot(),
-//     compact: false,
-//   }, 'node');
-// };
 
-rdforms.bundleLoader(itemStore, bundleDefault, null);
-// rdforms.bundleLoader(itemStore, bundleMo, callback);
+const callback = (bundles) => {
+  new rdforms.Editor({
+    graph,
+    resource: 'http://example.org/about',
+    template: bundles[0].getRoot(),
+    compact: false,
+  }, 'node');
+};
+
+rdforms.bundleLoader(itemStore, defaultBundles, callback);

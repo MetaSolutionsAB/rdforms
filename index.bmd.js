5d9f507fd6e00e84476b9f35bf198231a4b7fbb5
Valentino Hudhra <v.hudhra@gmail.com>
2018-11-20 11:18:21 +0100

    Add example7 with a choooser

--- index.bmd.js
+++ index.bmd.js
@@ -6,5 +6,6 @@ import Editor from './src/view/Editor';
 import Presenter from './src/view/Presenter';
 import ValidationPresenter from './src/view/ValidationPresenter';
 import bundleLoader from './src/template/bundleLoader';
+import renderingContext from './src/view/renderingContext';
 
-export {bundleLoader, ItemStore, Editor, Presenter, ValidationPresenter};
+export {bundleLoader, ItemStore, Editor, Presenter, ValidationPresenter, renderingContext};

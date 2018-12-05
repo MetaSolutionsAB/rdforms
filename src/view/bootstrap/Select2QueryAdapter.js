b7ff2573a5d5cee50e33a9e8921d02b7e82084ff
scazan <scottcazan@gmail.com>
2018-12-05 15:19:50 +0100

    Remove define

--- src/view/bootstrap/Select2QueryAdapter.js
+++ src/view/bootstrap/Select2QueryAdapter.js
@@ -1,38 +1,35 @@
 import utils from '../../utils';
+import ArrayAdapter from 'select2/src/js/select2/data/array';
+import Utils from 'select2/src/js/select2/utils';
 
-define([
-  'select2/src/js/select2/data/array', // TODO
-  'select2/src/js/select2/utils',
-], (ArrayAdapter, Utils) => {
-  const AjaxAdapter = function ($element, options) {
-    ArrayAdapter.__super__.constructor.call(this, $element, options);
-    this.rdformsItem = options.options.rdformsItem;
-    this.chooser = options.options.chooser;
+const AjaxAdapter = function ($element, options) {
+  ArrayAdapter.__super__.constructor.call(this, $element, options);
+  this.rdformsItem = options.options.rdformsItem;
+  this.chooser = options.options.chooser;
+};
+
+Utils.Extend(AjaxAdapter, ArrayAdapter);
+
+AjaxAdapter.prototype.query = function (params, callback) {
+  const request = () => {
+    this.chooser.search(this.rdformsItem, params.term).then((choices) => {
+      callback({ results: choices.map(c => ({
+        id: c.value,
+        text: utils.getLocalizedValue(c.label).value || '',
+        choice: c,
+      })) });
+    });
   };
 
-  Utils.Extend(AjaxAdapter, ArrayAdapter);
-
-  AjaxAdapter.prototype.query = function (params, callback) {
-    const request = () => {
-      this.chooser.search(this.rdformsItem, params.term).then((choices) => {
-        callback({ results: choices.map(c => ({
-          id: c.value,
-          text: utils.getLocalizedValue(c.label).value || '',
-          choice: c,
-        })) });
-      });
-    };
-
-    if (params.term !== '') {
-      if (this._queryTimeout) {
-        window.clearTimeout(this._queryTimeout);
-      }
-
-      this._queryTimeout = window.setTimeout(request, 300);
-    } else {
-      request();
+  if (params.term !== '') {
+    if (this._queryTimeout) {
+      window.clearTimeout(this._queryTimeout);
     }
-  };
 
-  return AjaxAdapter;
-});
+    this._queryTimeout = window.setTimeout(request, 300);
+  } else {
+    request();
+  }
+};
+
+export default AjaxAdapter;

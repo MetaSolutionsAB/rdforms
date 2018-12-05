8a5bc98a10c560f0fca833f35e61c4d5b16f1f05
scazan <scottcazan@gmail.com>
2018-12-05 11:17:18 +0100

    cleanup

--- index.no-bootstrap-css.js
+++ index.no-bootstrap-css.js
@@ -3,10 +3,7 @@ import './src/view/bootstrap/components';
 import './src/view/bootstrap/Select2'
 import './src/view/bootstrap/DateTimeFuelux';
 import 'jquery-mousewheel';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'fuelux/dist/css/fuelux.min.css';
 import 'select2/dist/css/select2.css';
-
 import './src/view/bootstrap/style.css';
 

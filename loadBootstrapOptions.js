5a6eb2a52aaea09f914b4d970f66d81b85d42d0f
scazan <scottcazan@gmail.com>
2019-04-18 15:27:56 +0200

    move two css imports back

--- loadBootstrapOptions.js
+++ loadBootstrapOptions.js
@@ -1,6 +1,9 @@
 /// #if BLOCKS
   import './src/view/bootstrap/Select2'
   import './src/view/bootstrap/DateTimeFuelux';
+  import 'fuelux/dist/css/fuelux.min.css';
+  import 'select2/dist/css/select2.css';
+
 /// #else
   import './src/view/bmd/all'; // hard coded bmd
 /// #endif

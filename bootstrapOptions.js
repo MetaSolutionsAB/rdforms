714db017176eec88701cebcba4ad198f4d3347e6
scazan <scottcazan@gmail.com>
2019-04-18 13:46:44 +0200

    updating conditional

--- bootstrapOptions.js
+++ bootstrapOptions.js
@@ -1,8 +1,8 @@
-// #if BLOCKS
+/// #if BLOCKS
   import './src/view/bootstrap/Select2'
   import './src/view/bootstrap/DateTimeFuelux';
   import './src/view/bootstrap/style.css';
-// #else
   import './src/view/bmd/all'; // hard coded bmd
-// #endif
+/// #else
+/// #endif
 

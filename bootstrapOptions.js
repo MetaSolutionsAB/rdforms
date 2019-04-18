60cd1846e01e29e43963c5efb3f013a065e03179
scazan <scottcazan@gmail.com>
2019-04-18 14:04:52 +0200

    adding tester

--- bootstrapOptions.js
+++ bootstrapOptions.js
@@ -3,6 +3,8 @@
   import './src/view/bootstrap/DateTimeFuelux';
   import './src/view/bootstrap/style.css';
   import './src/view/bmd/all'; // hard coded bmd
+console.log('hello');
 /// #else
+console.log('goodbye');
 /// #endif
 

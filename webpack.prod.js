8b608d75b0de4a8cd4da7bcebbc2662ee8835ca3
Valentino Hudhra <v.hudhra@gmail.com>
2019-06-17 10:28:25 +0200

    Make pipeline build

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = merge(common, {
   mode: 'production',

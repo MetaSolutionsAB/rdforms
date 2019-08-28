3b0f17f161c43b3ce7b2d397577527ccb6b71d9e
Matthias Palmer <matthias@metasolutions.se>
2019-08-28 02:23:24 +0200

    Only load bmd css when needed

--- loadBootstrapOptions.js
+++ loadBootstrapOptions.js
@@ -4,12 +4,11 @@
   import './src/view/bootstrap/DateTimeFuelux';
   import 'fuelux/dist/css/fuelux.min.css';
   import 'select2/dist/css/select2.css';
-
+  import './src/view/bootstrap/style.css';
 /// #else
   import 'core-js/stable';
   import 'regenerator-runtime/runtime';
   import './src/view/bmd/all'; // hard coded bmd
+  import './src/view/bmd/style.css';
 /// #endif
-
-import './src/view/bmd/style.css';
 

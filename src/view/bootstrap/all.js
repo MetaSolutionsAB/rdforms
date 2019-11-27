5c9de0311edf2fcdd179e0074195ea02b3fa3908
Valentino Hudhra <v.hudhra@gmail.com>
2019-11-27 13:13:27 +0100

    Add new datepicker dependency

--- src/view/bootstrap/all.js
+++ src/view/bootstrap/all.js
@@ -1,11 +1,13 @@
 import 'bootstrap';
 import './components';
 import './Select2'
-import './DateTimeFuelux';
+// import './DateTimeFuelux';
+import './DateTimeBootstrapPicker';
 import 'jquery-mousewheel';
 import 'sizzle';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'fuelux/dist/css/fuelux.min.css';
+// import 'fuelux/dist/css/fuelux.min.css';
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.standalone.min.css';
 import 'select2/dist/css/select2.css';
 import './style.css';
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,38 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  node: {
-    fs: 'empty',
-    process: false,
-  },
-  devServer: {
-    hot: true,
-  },
-});
+module.exports = (env, argv) => {
+  const type = argv.type ? argv.type : 'bmd';
+  const entry = {};
+  entry[type] = `index.${type}.js`;
+
+  return merge(common, {
+    entry,
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: "rdforms.[name].js",
+      library: 'rdforms',
+      libraryTarget: "umd"
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    plugins: [
+      new HtmlWebpackPlugin(), // Generates default index.html
+      new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'test.html',
+        template: 'src/assets/test.html'
+      })
+    ],
+    node: {
+      fs: 'empty',
+      process: false,
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      hot: true,
+      open: true,
+    },
+  });
+};
+
